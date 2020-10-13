ed569d2ab26a934b01e3b0bed6783e6f8bfdbfb4
Jermaine <jermzblake@gmail.com>
2020-10-13 14:46:17 -0400

    first commit including boilerplate html and css. along with cached elements for js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//cached input buttons
+let plusButton = document.getElementById("positive")
+let minusButton = document.getElementById("negative")
+//cached input field
+let inputField = document.querySelectorAll("input")
+inputField.defaultValue = "1"
+//cached counter
+let counterDisplay = document.getElementById("counter")
+
