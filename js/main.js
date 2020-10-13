32214ca90fff7a4cd9b461047bfaee8f9950a8a4
Jermaine <jermzblake@gmail.com>
2020-10-13 15:42:06 -0400

    add logic for functioning app

--- js/main.js
+++ js/main.js
@@ -2,8 +2,32 @@
 let plusButton = document.getElementById("positive")
 let minusButton = document.getElementById("negative")
 //cached input field
-let inputField = document.querySelectorAll("input")
-inputField.defaultValue = "1"
+let inputField = document.getElementById("number")
 //cached counter
 let counterDisplay = document.getElementById("counter")
+
+//app's state (variables)
+let counter = 0
+
+
+//event listners
+plusButton.addEventListener('click', addInput)
+minusButton.addEventListener('click', subtractInput)
+
+//functions
+function addInput(){
+    counter = (counter) + parseInt(inputField.value);
+    console.log(counter)
+     counterDisplay.textContent = counter;
+ 
+}
+
+
+function subtractInput(){
+    counter = (counter) - parseInt(inputField.value);
+    console.log(counter)
+     counterDisplay.textContent = counter;
+ 
+}
+
 
